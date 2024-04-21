91aecf0e1553990ef331b9654031c444cf7836e1
Branko Rajkovic <home.branko@gmail.com>
2024-04-22 01:36:30 +0200

    new upload to github

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -6387,7 +6387,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46741" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35665" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
